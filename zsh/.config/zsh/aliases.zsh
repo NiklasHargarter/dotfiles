b72cfe1942b251ec0084c09a6dd7de5d835d9fdc
Niklas Hargarter <niklashargarter@gmail.com>
2026-04-02 17:23:13 +0200

    cisco aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -2,3 +2,8 @@
 alias z='zellij'
 alias zwork='zellij attach -c work'
 alias zproj='zellij attach -c project'
+
+# Cisco VPN shortcuts
+alias vpnup='/opt/cisco/secureclient/bin/vpn -s < ~/.vpn-creds'
+alias vpndown='/opt/cisco/secureclient/bin/vpn disconnect'
+alias vpnstat='/opt/cisco/secureclient/bin/vpn stats'
